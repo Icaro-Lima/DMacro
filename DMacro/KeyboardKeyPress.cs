9098976699555a950f2563659be2774c5a7178d5
IcaroLima <icaro.lima@ccc.ufcg.edu.br>
2017-12-26 03:33:45 -0300

    Adicionados mais dois eventos. -Adicionado classe KeyboardKeyPress. -Adicionado classe Wait.

---
+++ DMacro/KeyboardKeyPress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using WindowsInput.Native;
+
+namespace DMacro
+{
+    class KeyboardKeyPress : KeyboardInput, IEvent
+    {
+        private VirtualKeyCode VirtualKeyCode;
+
+        public KeyboardKeyPress(string key)
+        {
+            VirtualKeyCode = (VirtualKeyCode)Enum.Parse(typeof(VirtualKeyCode), key);
+        }
+
+        public void Run(Dictionary<string, object> variables)
+        {
+            KeyboardSimulator.KeyPress(VirtualKeyCode);
+        }
+    }
+}
---
+++ DMacro/Wait.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMacro
+{
+    class Wait : IEvent
+    {
+        private int Milliseconds;
+
+        public Wait(int milliseconds)
+        {
+            Milliseconds = milliseconds;
+        }
+
+        public void Run(Dictionary<string, object> variables)
+        {
+            System.Threading.Thread.Sleep(Milliseconds);
+        }
+    }
+}
