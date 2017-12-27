bcb3be362533dbcb1cd553aac938391abf143d37
IcaroLima <icaro.lima@ccc.ufcg.edu.br>
2017-12-26 23:06:55 -0300

    Adicionado a classe MouseMove.

---
+++ DMacro/Events/MouseMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DMacro
+{
+    class MouseMove : MouseInput, IEvent
+    {
+        private Point Point;
+
+        public MouseMove(Point point)
+        {
+            Point = point;
+        }
+
+        public int Run(Dictionary<string, object> variables)
+        {
+            MouseSimulator.MoveMouseTo(Point.X / Screen.PrimaryScreen.Bounds.Width * ushort.MaxValue, Point.Y / Screen.PrimaryScreen.Bounds.Height * ushort.MaxValue);
+
+            return -1;
+        }
+    }
+}
